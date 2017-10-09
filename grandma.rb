2d2ce37f78357b81d66bcdb362cdcb06349c218e
Bethany Shao <bethany.magdalena@gmail.com>
2017-10-09 01:49:50 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,11 @@
 
 def speak_to_grandma(phrase)
 
-  if phrase.upcase != phrase
+  if phrase == "I LOVE YOU GRANDMA!"
+    "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase.upcase == phrase
+    "NO, NOT SINCE 1938!"
+  else
     "HUH?! SPEAK UP, SONNY!"
   end
 
